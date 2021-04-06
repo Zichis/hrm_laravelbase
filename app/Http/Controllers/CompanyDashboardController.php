46c7c93d2a10dff8d90340503658d8cda0f2deb5
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-07 00:21:48 +0100

    Pass company data to view.

--- app/Http/Controllers/CompanyDashboardController.php
+++ app/Http/Controllers/CompanyDashboardController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CompanyDashboardController extends Controller
 {
     public function index(string $company)
     {
-        return view('company.dashboard.index');
+        $company = Company::where('identifier', $company)->firstOrFail();
+        if (Auth::user()->company != $company) {
+            abort('404');
+        }
+        return view('company.dashboard.index', [
+            'company' => $company
+        ]);
     }
 }
