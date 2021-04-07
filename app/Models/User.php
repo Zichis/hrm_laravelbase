6a3723b1aeb26c4e7ae725abf2f0615b7194518a
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-07 12:16:12 +0100

    Updated user model with personal relationship.

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Company::class);
     }
+
+    public function personal()
+    {
+        return $this->hasOne(Personal::class);
+    }
 }
