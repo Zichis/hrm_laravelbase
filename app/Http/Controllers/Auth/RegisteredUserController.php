ef5a25a15749e102fada8ef4013a9bff7a981e0c
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-07 11:38:03 +0100

    Fixed unique company name.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,7 +37,7 @@ class RegisteredUserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|confirmed|min:8',
-            'company_name' => 'required|regex:/^[\pL\s]+$/u'
+            'company_name' => 'required|regex:/^[\pL\s]+$/u|unique:companies,name'
         ]);
 
         $company = Company::create([
--- database/migrations/2021_04_05_191439_create_companies_table.php
+++ database/migrations/2021_04_05_191439_create_companies_table.php
@@ -15,8 +15,8 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('identifier');
+            $table->string('name')->unique();
+            $table->string('identifier')->unique();
             $table->timestamps();
         });
     }
