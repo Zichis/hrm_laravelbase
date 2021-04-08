7621d36d07bd3afe8ed73905ac8b2d9a61de328f
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-08 11:43:24 +0100

    Added destroy link.

--- resources/views/company/staff/show.blade.php
+++ resources/views/company/staff/show.blade.php
@@ -9,7 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    {{ $staff->name }} <a class="font-bold text-red-500 hover:text-red-700" href="{{ route('staff.edit', ['company' => $company->identifier, 'staff' => $staff->id]) }}">Edit</a>
+                    {{ $staff->name }}
+                    <a class="font-bold text-blue-500 hover:text-blue-700" href="{{ route('staff.edit', ['company' => $company->identifier, 'staff' => $staff->id]) }}">Edit</a>
+                    <form method="POST" action="{{ route('staff.destroy', ['company' => $company->identifier, 'staff' => $staff->id]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="font-bold text-red-500 hover:text-red-700" >Delete</button>
+                    </form>
                 </div>
             </div>
         </div>
