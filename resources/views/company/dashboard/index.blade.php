27b7d06e900fdc9fbf2853fa5e73315e330febf1
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-08 23:58:58 +0100

    Implemented simple layout.

--- resources/views/company/dashboard/index.blade.php
+++ resources/views/company/dashboard/index.blade.php
@@ -1,18 +1,7 @@
-<x-app-layout>
+<x-dashboard-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ $company->name }} {{ __('Dashboard') }}
-        </h2>
+        <h1>Header goes here</h1>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <p>You're logged in!</p>
-                    <p>Visit staff <a class="font-bold text-blue-500 hover:text-blue-700" href="{{ route('staff.index', $company->identifier) }}">page</a></p>
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+    <p>Hmm...</p>
+</x-dashboard-layout>
