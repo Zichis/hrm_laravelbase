3e26d7ba442565b4a5f6767ffbfe7c4c1c49b1de
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-08 11:21:40 +0100

    Created edit form.

---
+++ resources/views/company/staff/edit.blade.php
@@ -0,0 +1,56 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ auth()->user()->company->name }} {{ __('Staff') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <!-- Validation Errors -->
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+                    <form method="POST" action="{{ route('staff.update', ['company' => $company->identifier, 'staff' => $staff->id]) }}">
+                        @csrf
+                        @method('PUT')
+                        <!-- Name -->
+                        <div class="mt-4">
+                            <x-label for="name" :value="__('Name')" />
+
+                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="$staff->name" required />
+                        </div>
+
+                        <!-- First Name -->
+                        <div class="mt-4">
+                            <x-label for="first_name" :value="__('First Name')" />
+
+                            <x-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="$staff->personal->first_name" required />
+                        </div>
+
+                        <!-- Last Name -->
+                        <div class="mt-4">
+                            <x-label for="last_name" :value="__('Last Name')" />
+
+                            <x-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="$staff->personal->last_name" required />
+                        </div>
+
+                        <!-- Email Address -->
+                        <div class="mt-4">
+                            <x-label for="email" :value="__('Email')" />
+
+                            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="$staff->email" required />
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <x-button class="ml-4">
+                                {{ __('Update') }}
+                            </x-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
