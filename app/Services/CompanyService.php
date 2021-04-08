1b3ccebc128da198b71a65c988f499a5ee5ef479
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-08 10:35:31 +0100

    Updated isCompany staff Check if user is from company.

--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -3,13 +3,20 @@
 namespace App\Services;
 
 use App\Models\Company;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class CompanyService {
-    public function isCompanyStaff(Company $company): bool
+    public function isCompanyStaff(Company $company, ?User $user = null): bool
     {
         if (Auth::user()->company == $company) {
             return true;
+        }
+
+        if (!is_null($user) && $user instanceof User) {
+            if ($user->company == $company) {
+                return true;
+            }
         }
 
         return false;
