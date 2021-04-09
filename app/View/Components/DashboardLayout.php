cfdadbb6f225200fb0835abb02720af1a36a388e
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-09 23:32:10 +0100

    Added company variable to component.

--- app/View/Components/DashboardLayout.php
+++ app/View/Components/DashboardLayout.php
@@ -6,14 +6,16 @@ use Illuminate\View\Component;
 
 class DashboardLayout extends Component
 {
+    public $company;
+
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($company)
     {
-        //
+        $this->company = $company;
     }
 
     /**
