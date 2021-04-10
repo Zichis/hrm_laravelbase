36a9e7f0b548d8090f01c533bb855d78dbcf1acc
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-10 15:29:54 +0100

    Added dashboard content.

--- resources/views/company/dashboard/index.blade.php
+++ resources/views/company/dashboard/index.blade.php
@@ -1,7 +1,12 @@
-<x-dashboard-layout>
+<x-dashboard-layout :company=$company>
     <x-slot name="header">
-        <h1>Header goes here</h1>
+        <div class="px-10 py-5">
+            <h1 class="text-3xl">{{ auth()->user()->personal->first_name }} {{ auth()->user()->personal->last_name }}</h1>
+            <p>{{ auth()->user()->company->name }}</p>
+        </div>
     </x-slot>
 
-    <p>Hmm...</p>
+    <div class="px-10 py-5">
+        <h1>Dashboard</h1>
+    </div>
 </x-dashboard-layout>
