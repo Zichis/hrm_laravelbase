c593d5e5477c3dbd82838056077684d56e24031b
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-10 20:57:53 +0100

    Used dashboard layout in create page.

--- resources/views/company/staff/create.blade.php
+++ resources/views/company/staff/create.blade.php
@@ -1,10 +1,4 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ auth()->user()->company->name }} {{ __('Staff') }}
-        </h2>
-    </x-slot>
-
+<x-dashboard-layout :company=$company>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -60,4 +54,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-dashboard-layout>
