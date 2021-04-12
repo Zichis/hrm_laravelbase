08559b2903d2efb6859154eeaab0534c04c0cc68
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-12 22:47:13 +0100

    Reduced padding.

--- resources/views/company/staff/create.blade.php
+++ resources/views/company/staff/create.blade.php
@@ -1,5 +1,5 @@
 <x-dashboard-layout :company=$company>
-    <div class="py-12">
+    <div class="py-6">
         <div class="w-full sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <h1 class="text-2xl p-6">New staff</h1>
