00bc15f44de1e8183c00b917204fac1762a30c0c
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-04-12 22:51:21 +0100

    Keep user icon active on create.

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ route('company.dashboard', ['company'=> $company->identifier]) }}" class="p-2 block my-5 @if(Request::url() == route('company.dashboard', ['company' => $company->identifier])) text-green-600 @endif hover:text-green-600 focus:outline-none">
             <i class="fas fa-th-large"></i>
         </a>
-        <a href="{{ route('staff.index', ['company'=> $company->identifier]) }}" class="p-2 block my-5 @if(Request::url() == route('staff.index', ['company' => $company->identifier])) text-green-600 @endif hover:text-green-600 focus:outline-none">
+        <a href="{{ route('staff.index', ['company'=> $company->identifier]) }}" class="p-2 block my-5 @if(Request::url() == route('staff.index', ['company' => $company->identifier]) || Request::url() == route('staff.create', ['company' => $company->identifier])) text-green-600 @endif hover:text-green-600 focus:outline-none">
             <i class="fas fa-users"></i>
         </a>
     </div>
